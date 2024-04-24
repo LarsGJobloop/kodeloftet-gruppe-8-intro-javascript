5fc01482d5d704a4c6e77817b0a6d57cabc8a1de
LarsGJobloop <lars.gunnar@jobloop.no>
2024-04-24 10:44:14 +0200

    showcase linking and variable declaration

---
+++ main.js
@@ -0,0 +1,24 @@
+// Definer en variabel
+let greeting
+// Gi den en verdi
+greeting = "En vilkårlig verdi"
+
+// Gjøre noe med den variabelen
+console.log(greeting)
+
+// Omdefinere verdien til variabelen (gi den en ny verdi)
+greeting = "En helt ny verdi"
+console.log(greeting)
+
+// Definer konstanter
+const constantVariabel = "I am Constant"
+
+// Nest linje crasher progammet
+// constantVariabel = "This does not work"
+
+/**
+ * Flere linjer med 
+ * kommentarer
+ */
+
+
